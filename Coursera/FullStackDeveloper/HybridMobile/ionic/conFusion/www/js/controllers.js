998094e4387233d0c1818f432916f2d278f981b5
PINHOT <tiago.pinho@theaa.ie>
2016-02-26 17:17:42 +0000

    week 1 lesson 6 - 2

--- Coursera/FullStackDeveloper/HybridMobile/ionic/conFusion/www/js/controllers.js
+++ Coursera/FullStackDeveloper/HybridMobile/ionic/conFusion/www/js/controllers.js
@@ -41,7 +41,9 @@ angular.module('conFusion.controllers', [])
   };
 })
 
-.controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+.controller('MenuController', ['$scope', 'menuFactory', 'baseURL', function($scope, menuFactory, baseURL) {
+
+            $scope.baseURL = baseURL;
             
             $scope.tab = 1;
             $scope.filtText = '';
@@ -117,7 +119,9 @@ angular.module('conFusion.controllers', [])
             };
         }])
 
-        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
+ .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', 'baseURL', function($scope, $stateParams, menuFactory, baseURL) {
+            
+			$scope.baseURL = baseURL;
             
             $scope.dish = {};
             $scope.showDish = false;
