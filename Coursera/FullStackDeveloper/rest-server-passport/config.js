b5e2b992812e817efbfc901d3ef76648f47e4108
tagus24 <tiago.filipe.pinho@gmail.com>
2016-03-19 17:44:09 +0000

    without conf file

--- Coursera/FullStackDeveloper/rest-server-passport/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
-    'facebook': {
-        clientID: '467168890138613',
-        clientSecret: 'f300647d7234d9ec8919b84186d6c790',
-        callbackURL: 'https://localhost:3443/users/facebook/callback'
-    }
-}
